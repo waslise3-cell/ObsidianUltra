600e8525dc73bbfe319638af0ebf5924c1a1e800
waslise3-cell <waslise3@gmail.com>
2026-09-12 23:23:13 +0500

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -19,7 +19,7 @@
 
 --// 1. Bootstrap \\--
 
-local Repo = "https://raw.githubusercontent.com/Naellx/ObsidianUltra/main/"
+local Repo = "https://github.com/waslise3-cell/ObsidianUltra/blob/main/"
 local Library = loadstring(game:HttpGet(Repo .. "Library.lua"))()
 local ThemeManager = loadstring(game:HttpGet(Repo .. "addons/ThemeManager.lua"))()
 local SaveManager = loadstring(game:HttpGet(Repo .. "addons/SaveManager.lua"))()
